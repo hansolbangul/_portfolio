fb24c62c89d4b481ade7b7acddb057a2c2036f63
hansolbangul <hansolbangul@farmos.co.kr>
2022-04-27 15:25:21 +0900

    컴포넌트 수정

--- src/component/Experience.tsx
+++ src/component/Experience.tsx
@@ -5,15 +5,32 @@ import styled from 'styled-components';
 
 const SectionDiv = styled.div`
   width: 100%;
-  height: 50vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
   background-color: #4fed5c;
+`;
+
+
+
+const Title = styled.h1`
+  display: inline;
+  font-size: 2.8rem;
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  font-weight: 900;
+  margin: 0 auto 3rem;
+  line-height: 1.5;
 `;
 
 type DivProps = JSX.IntrinsicElements['div'];
 
 const Experience = React.forwardRef<HTMLDivElement, DivProps>((props, ref) => (
   <>
-    <SectionDiv {...props} ref={ref}></SectionDiv>
+    <SectionDiv {...props} ref={ref}>
+      <Title>Header</Title>
+      <div>hello</div>
+    </SectionDiv>
   </>
 ));
 
--- src/component/Stack.tsx
+++ src/component/Stack.tsx
@@ -3,15 +3,30 @@ import styled from 'styled-components';
 
 const SectionDiv = styled.div`
   width: 100%;
-  height: 50vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
   background-color: #ed4fe0;
+`;
+
+const Title = styled.h1`
+  display: inline;
+  font-size: 2.8rem;
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  font-weight: 900;
+  margin: 0 auto 3rem;
+  line-height: 1.5;
 `;
 
 type DivProps = JSX.IntrinsicElements['div'];
 
 const Stack = React.forwardRef<HTMLDivElement, DivProps>((props, ref) => (
   <>
-    <SectionDiv {...props} ref={ref}></SectionDiv>
+    <SectionDiv {...props} ref={ref}>
+      <Title>Stack</Title>
+      <div>hello</div>
+    </SectionDiv>
   </>
 ));
 
